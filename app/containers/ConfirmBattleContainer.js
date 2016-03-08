59d307d2453cc3349b466a38cdc841ff19541313
andela-sjames <samuel.james@andela.com>
2016-03-08 21:58:48 +0100

    add route to initiate battle and userdetail component

--- app/containers/ConfirmBattleContainer.js
+++ app/containers/ConfirmBattleContainer.js
@@ -26,10 +26,20 @@ var ConfirmBattleContainer = React.createClass({
       }.bind(this))
   },
 
+  handleInitiateBattle: function() {
+    this.context.router.push({
+      pathname: '/results',
+      state: {
+        playersInfo: this.state.playersInfo
+      }
+    })
+  },
+
   render: function () {
     return (
       <ConfirmBattle
         isLoading={this.state.isLoading}
+        onInitiateBattle={this.handleInitiateBattle}
         playersInfo={this.state.playersInfo} />
     )
   }
@@ -37,5 +47,3 @@ var ConfirmBattleContainer = React.createClass({
 });
 
 module.exports = ConfirmBattleContainer;
-
-
